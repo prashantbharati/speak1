52b89441464e9f5eec8de34b1f356590fd647d61
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-13 22:28:52 +0530

    nav done

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,4 @@
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import logo from "./images/Logo.jpg";
 const Nav = () => {
@@ -9,12 +9,79 @@ const Nav = () => {
           <div
             style={{
               backgroundImage: `url(${logo})`,
-              backgroundSize: "contain",
+              backgroundSize: "100% 100%",
               backgroundRepeat: "no-repeat",
               height: "10vh",
             }}
+          ></div>
+        </Grid>
+
+        <Grid item sm={2}></Grid>
+
+        <Grid item={7}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "10vh",
+            }}
           >
-            Nav
+            <Typography
+              style={{
+                fontFamily: "Poppins",
+                fontSize: "20px",
+
+                color: "white",
+                fontWeight: "100",
+              }}
+            >
+              Home
+            </Typography>
+            <Typography
+              style={{
+                fontFamily: "Poppins",
+                fontSize: "20px",
+                marginLeft: "50px",
+                color: "white",
+                fontWeight: "300",
+              }}
+            >
+              About us
+            </Typography>
+            <Typography
+              style={{
+                fontFamily: "Poppins",
+                fontSize: "20px",
+                marginLeft: "50px",
+                color: "white",
+                fontWeight: "300",
+              }}
+            >
+              Shop
+            </Typography>{" "}
+            <Typography
+              style={{
+                fontFamily: "Poppins",
+                fontSize: "20px",
+                marginLeft: "50px",
+                color: "black",
+                fontWeight: "300",
+              }}
+            >
+              Support
+            </Typography>{" "}
+            <Typography
+              style={{
+                fontFamily: "Poppins",
+                fontSize: "20px",
+                marginLeft: "50px",
+                color: "black",
+                fontWeight: "300",
+              }}
+            >
+              Contact us
+            </Typography>
           </div>
         </Grid>
       </Grid>
