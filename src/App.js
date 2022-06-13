65ee502a40f6b430d479f47776616d94ba575e75
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-13 18:58:16 +0530

    img 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import bg4 from "./images/Bg4.png";
+import grp from "./images/Group.jpg";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          backgroundImage: `url(${bg4})`,
+          backgroundRepeatX: "no-repeat",
+          backgroundSize: "50% 120%",
+          height: "100vh",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            backgroundImage: `url(${grp})`,
+            backgroundRepeatX: "no-repeat",
+            backgroundSize: "50% 120%",
+            height: "100vh",
+            width: "100%",
+          }}
+        ></div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
