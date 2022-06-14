162f5b55b9d946d25e103aed46d919c1c0c7f38b
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-14 07:08:50 +0530

    div 3 done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import bg4 from "./images/Bg4.png";
 import grp from "./images/Group.jpg";
 import grp2 from "./images/Group2.jpg";
 import rect2 from "./images/Rectangle2.png";
+import e3 from "./images/Ellipse3.jpg";
+import customer from "./images/Customer.png";
 import { Grid, Container, Typography, Button } from "@material-ui/core";
 import "./index.css";
 import Nav from "./Nav";
@@ -174,7 +176,83 @@ const App = () => {
           height: "100vh",
           width: "100%",
         }}
-      ></div>
+      >
+        <Grid container>
+          <Grid item sm={1}></Grid>
+          <Grid item sm={5}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+
+                height: "100vh",
+              }}
+            >
+              <Typography
+                style={{
+                  fontFamily: "Poppins",
+                  fontSize: "60px",
+
+                  color: "black",
+                }}
+              >
+                MEET YOUR TUTOR
+              </Typography>
+
+              <Typography
+                style={{
+                  fontFamily: "Poppins",
+                  fontSize: "25px",
+                  fontWeight: "200",
+                  color: "black",
+                }}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item sm={1}></Grid>
+          <Grid item sm={5}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+              }}
+            >
+              <div
+                style={{
+                  position: "relative",
+                  height: "60%",
+                  width: "100%",
+                  backgroundImage: `url(${e3})`,
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                }}
+              >
+                <div
+                  style={{
+                    position: "absolute",
+                    top: "20px",
+                    left: "45px",
+                    height: "80%",
+                    width: "50%",
+                    backgroundImage: `url(${customer})`,
+                    backgroundSize: "contain",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
     </>
   );
 };
