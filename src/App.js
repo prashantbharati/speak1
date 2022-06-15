b14750baaf1f854129d3d78694b5a6051670ef06
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-15 23:06:59 +0530

    middle backgroud img done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import bg3 from "./images/Bg3.png";
 import bg4 from "./images/Bg4.png";
 import grp from "./images/Group.png";
 import grp2 from "./images/Group2.jpg";
@@ -22,94 +23,102 @@ const App = () => {
     <>
       <div
         style={{
-          backgroundImage: `url(${bg4})`,
+          backgroundImage: `url(${bg3})`,
           backgroundRepeatX: "no-repeat",
-          backgroundSize: "60% 120%",
+          backgroundSize: "63% 120%",
           height: "100vh",
           width: "100%",
         }}
       >
-        <Nav />
-        <Grid container>
-          <Grid item sm={4}>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "90vh",
-                fontSize: "70px",
-                color: "white",
-              }}
-            >
-              <Typography
-                style={{
-                  fontFamily: "Poppins",
-                  fontSize: "70px",
-                  textAlign: "center",
-                }}
-              >
-                {" "}
-                Study
-              </Typography>
-
-              <Typography
-                style={{
-                  fontFamily: "Poppins",
-                  fontSize: "70px",
-                  textAlign: "center",
-                }}
-              >
-                {" "}
-                with us!
-              </Typography>
-              <Typography
-                style={{
-                  fontFamily: "Poppins",
-                  fontSize: "30px",
-                  marginLeft: "40px",
-                }}
-              >
-                Learn english easily on one click from your house.
-              </Typography>
-
-              <Button
-                style={{
-                  backgroundColor: "#FFB750",
-                  color: "black",
-                  marginTop: "30px",
-                }}
-                variant="contained"
-              >
-                Know More
-              </Button>
-            </div>
-          </Grid>
-
-          <Grid item sm={0}></Grid>
-
-          <Grid item sm={5}>
-            <div
-              style={{
-                display: "flex",
-
-                height: "90vh",
-              }}
-            >
+        <div
+          style={{
+            backgroundImage: `url(${bg4})`,
+            backgroundRepeatX: "no-repeat",
+            backgroundSize: "60% 120%",
+            height: "100vh",
+            width: "100%",
+          }}
+        >
+          <Nav />
+          <Grid container>
+            <Grid item sm={4}>
               <div
                 style={{
-                  backgroundImage: `url(${grp})`,
-                  backgroundRepeatX: "no-repeat",
-                  backgroundSize: "contain",
-                  height: "90%",
-                  width: "100%",
-                  marginLeft: "20px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "90vh",
+                  fontSize: "70px",
+                  color: "white",
                 }}
-              ></div>
-            </div>
+              >
+                <Typography
+                  style={{
+                    fontFamily: "Poppins",
+                    fontSize: "70px",
+                    textAlign: "center",
+                  }}
+                >
+                  {" "}
+                  Study
+                </Typography>
+
+                <Typography
+                  style={{
+                    fontFamily: "Poppins",
+                    fontSize: "70px",
+                    textAlign: "center",
+                  }}
+                >
+                  {" "}
+                  with us!
+                </Typography>
+                <Typography
+                  style={{
+                    fontFamily: "Poppins",
+                    fontSize: "30px",
+                    marginLeft: "40px",
+                  }}
+                >
+                  Learn english easily on one click from your house.
+                </Typography>
+
+                <Button
+                  style={{
+                    backgroundColor: "#FFB750",
+                    color: "black",
+                    marginTop: "30px",
+                  }}
+                  variant="contained"
+                >
+                  Know More
+                </Button>
+              </div>
+            </Grid>
+
+            <Grid item sm={5}>
+              <div
+                style={{
+                  display: "flex",
+
+                  height: "90vh",
+                }}
+              >
+                <div
+                  style={{
+                    backgroundImage: `url(${grp})`,
+                    backgroundRepeatX: "no-repeat",
+                    backgroundSize: "contain",
+                    height: "90%",
+                    width: "100%",
+                    marginLeft: "20px",
+                  }}
+                ></div>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
       </div>
       {/*div 1 ends*/}
 
